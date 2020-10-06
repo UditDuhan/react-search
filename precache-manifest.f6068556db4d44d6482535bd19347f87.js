2fe3e2dd4633e18f5b830e72f40c04346eebf836
UditDuhan <uditduhan23@gmail.com>
2020-10-06 16:56:10 +0530

    Updates

---
+++ precache-manifest.f6068556db4d44d6482535bd19347f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2550d0375f90365dff767c74dd12b5c",
+    "url": "/react-search/index.html"
+  },
+  {
+    "revision": "c6234228ef87db77d041",
+    "url": "/react-search/static/css/main.9c6cdbf9.chunk.css"
+  },
+  {
+    "revision": "f554f317bf388566fcce",
+    "url": "/react-search/static/js/2.d13672f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-search/static/js/2.d13672f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6234228ef87db77d041",
+    "url": "/react-search/static/js/main.3db800c3.chunk.js"
+  },
+  {
+    "revision": "028118f2f42e23655082",
+    "url": "/react-search/static/js/runtime-main.96f1e394.js"
+  }
+]);
