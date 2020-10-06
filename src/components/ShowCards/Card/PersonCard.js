98c64dc4e5d273a1058abd821989a65ac4e07b27
Mohit kumar <mohitpalk2728@gmail.com>
2020-10-06 16:29:42 +0530

    m

--- src/components/ShowCards/Card/PersonCard.js
+++ src/components/ShowCards/Card/PersonCard.js
@@ -5,13 +5,13 @@ const Card = (props) => {
   return (
     <div className={classes.Card}>
       <img src={props.person.avatar} alt={props.person.first_name} />
-      <p>
+      <p className={classes.PersonName}>
         Name:{" "}
         <strong>
           {props.person.first_name} {props.person.last_name}
         </strong>
       </p>
-      <p>
+      <p className={classes.PersonEmail}>
         Email:{" "}
         <strong>
           <em>{props.person.email}</em>
